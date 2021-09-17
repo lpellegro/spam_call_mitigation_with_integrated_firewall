f109ea9bb6a35c8bf2e28283268f6c52e12b5807
Luca Pellegrini <nepero@gmail.com>
2021-09-17 11:42:50 +0200

    Updated credentials

--- credentials.py
+++ credentials.py
@@ -14,23 +14,10 @@ Created on Fri Jun 25 16:39:35 2021
 credentials = {#EXPRESSWAY SECTION
                #Cluster 1
                'expe_cluster1':'expe1a.example.com', #FQDN of cluster1 primary peer
-               'url_cluster1':'https://expe1a.example.com', #URL for cluster1 (usually to the primary peer); the URL should include the port if different from 443 
-               'expe1a.example.com':['admin', 'password1a', dual_interface], #username and password for all peers in all clusters
-               'expe1b.example.com':['admin', 'password1b', dual_interface], #dual_interface set to True if Expressway has dual NIC, False if single NIC (see below)
+               'expe1a.example.com':['admin', 'password1a', dual_interface, primary_peer, admin_port], #username and password for all peers in all clusters, dual_interface set to True if Expressway has dual NIC, False if single NIC (see below)
+               'expe1b.example.com':['admin', 'password1b', dual_interface, primary_peer, admin_port], #add the primary peer for that cluster, and the web interface admin_port. 443 is the default; if modified, use the changed value (see below)
                '192.0.2.201':'expe1a.example.com', #IP addresses for all peers in all clusters
                '192.0.2.202':'expe1b.example.com',
-
-
-               #Cluster 2 (remove if not present)
-               'expe_cluster2':'expe2a.example.com', #FQDN of cluster2 primary peer; add more lines if more clusters are involved 
-               'url_cluster2':'https://expe2a.example.com:7443', #URL for cluster2 (includes port 7443 as admin HTTPS traffic uses this port)
-               'expe2a.example.com':['admin', 'password2a', dual_interface],
-               'expe2b.example.com':['admin', 'password2b', dual_interface],
-               'expe2c.example.com':['admin', 'password2c', dual_interface],
-               '203.0.113.201':'expe2a.example.com',
-               '203.0.113.202':'expe2b.example.com',
-               '203.0.113.203':'expe2c.example.com',
-
                #Add more clusters if required
                #......
                #CLOUD SECTION
@@ -49,8 +36,7 @@ credentials = {#EXPRESSWAY SECTION
 """ Single peer cluster with single NIC and user 'administrator' and password 'mysecret' that uses port 7443 as admin port
 
 credentials = {'expe_cluster1':'expe.example.com', 
-               'url_cluster1':'https://expe.example.com:7443',
-               'expe.example.com':['administrator', 'mysecret', False], 
+               'expe.example.com':['administrator', 'mysecret', False, 'expe.example.com', '7443'], 
                '192.0.2.301':'expe.example.com',
                'roomID':'abcdefghilmnopqrstuvz0123456789abcdefghilmnopqrstuvz0123456789abcdefghilmnopqrstuvz0123456789',
                'bearer':'MNBVCXZLKJGHGFDSAPOIUYTREWQMNBVCXZLKJGHGFDSAPOIUYTREWQMNBVCXZLKJGHGFDSAPOIUYTREWQ-09876-54321-012345678',
@@ -63,17 +49,15 @@ credentials = {'expe_cluster1':'expe.example.com',
 Two clusters, 2 and 3 peers with dual NIC, one with standard admin port and another using 7443. Same username and passwords.
 
 credentials = {'expe_cluster1':'expe1a.example.com', 
-               'url_cluster1':'https://expe1a.example.com',
-               'expe1a.example.com':['admin', 'password1a', True], 
-               'expe1b.example.com':['admin', 'password1b', True],  
+               'expe1a.example.com':['admin', 'password1a', True, 'expe1a.example.com', '7443'], 
+               'expe1b.example.com':['admin', 'password1b', True, 'expe1a.example.com', '7443'],  
                '192.0.2.201':'expe1a.example.com', 
                '192.0.2.202':'expe1b.example.com', 
                
                'expe_cluster2':'expe2a.example.com', 
-               'url_cluster2':'https://expe2a.example.com:7443', 
-               'expe2a.example.com':['admin', 'password2a', True], 
-               'expe2b.example.com':['admin', 'password2b', True], 
-               'expe2c.example.com':['admin', 'password2c', True],
+               'expe2a.example.com':['admin', 'password2a', True, 'expe2a.example.com', '443'], 
+               'expe2b.example.com':['admin', 'password2b', True, 'expe2a.example.com', '443'], 
+               'expe2c.example.com':['admin', 'password2c', True, 'expe2a.example.com', '443'],
                '203.0.113.201':'expe2a.example.com',
                '203.0.113.202':'expe2b.example.com',
                '203.0.113.203':'expe2c.example.com',
